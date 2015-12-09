50f29425d758aa6c9e158bc8945f3e44b8dccfad
JiaLingLiu1994 <jenny26096@gmail.com>
2015-12-09 21:45:30 +0800

    Add js code(meal_modal)

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,12 @@
 window.onload = eventLoad;
 
 $(function(){
-	$("#meal-list img").click(function(){
-		var N = $(this).attr("id").substr(2);
-	})
-})
+	$("#photoList img").click(function(){
+
+		var N = $(this).attr("id").substr(3);
+
+		//var Photo_Path = $(this).attr("src");
+
+		$("#mainPhoto").attr( "src", "img/meal.jpg");
+	});
+});
